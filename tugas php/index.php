ae68925cb2ea8ab833dc07ee2baf3a04d35785b1
holan <muntiholan88@gmail.com>
2023-02-11 15:34:08 +0800

    mengatur folder repo

---
+++ tugas php/index.php
@@ -0,0 +1,15 @@
+
+    <?php
+
+    $a=2;
+    $b=3;
+    $c=4;
+//luas persegi panjang
+    $d=$a*$b;
+    echo $a*$b=$c;
+    echo 'holan';
+//luas segi tiga
+$d=$a*$b/2;
+echo $c*$b*$a=$d;
+//
+    ?>
