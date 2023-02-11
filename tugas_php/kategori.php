e984ac7cce03315904f3ce4887f58e1497aad1c4
holan <muntiholan88@gmail.com>
2023-02-11 17:07:39 +0800

    mengumulkan tugas kategori.php

---
+++ tugas_php/kategori.php
@@ -0,0 +1,19 @@
+<?php
+$name = "Holan";
+  $weight = "55"; 
+  // berat badan dalam kilogram
+  $height = "1.65"; 
+  // tinggi badan dalam meter
+
+  $bmi = $weight / ($height * $height);
+
+  if ($bmi < 18.5) {
+    echo "Hallo ,$name . niali BMI anda adalah $bmi anda termasuk kurus";
+  } elseif ($bmi >= 18.5 && $bmi < 25) {
+    echo "Hallo ,$name . niali BMI anda adalah $bmi anda termasuk sedang";
+  } elseif ($bmi >= 25 && $bmi < 30) {
+    echo "Hallo ,$name . niali BMI anda adalah $bmi anda termasuk gemuk";
+  } else {
+    echo "Hallo ,$name . niali BMI anda adalah $bmi anda termasuk Obesitas";
+  }
+?>
