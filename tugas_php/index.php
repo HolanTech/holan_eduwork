c75131e87ee795fcba1a080645f483a762710ef3
holan <muntiholan88@gmail.com>
2023-02-11 16:32:12 +0800

    menambahkan tugas_php

---
+++ tugas_php/index.php
@@ -0,0 +1,45 @@
+
+    <?php
+
+    $a=20;
+    $b=30;
+    $c=40;
+//luas persegi panjang
+    $d=$a*$b;
+    echo "$a*$b =$d";
+    echo "<hr>";
+//luas persegi
+    $d=$a*$a;
+    echo "$a*$a =$d";
+    echo "<hr>";
+//luar segitiga
+    $d=$b*$b/2;
+    echo"$b*$b/2=$d";
+    echo"<hr>";
+//luar bileh ketupat
+//diagonal 1 =40 dan 2 =30
+    $d=$c*$b/2;
+    echo "$c*$b/2=$d";
+    echo "<hr>";
+//luas lingkaran
+//jari-jari=20
+    $d = 3.14 * $a ** 2;
+    echo "3.14*$a**2*=$d";
+    echo "<hr>";
+//Kubus
+//dimana sisi=20
+    $d = $a **3;
+    echo "$a**3=$d";
+    echo "<hr>";
+//Balok: panjang=c lebar=b tinggi =a
+    $d = $c * $b * $a;
+    echo "$c*$b*$a=$d";
+    echo "<hr>";
+
+//Prisma Segitiga: panjang =a lebar =c dan tinggi =b
+    $d = ($a * $c / 2) / 2;
+    echo "($a*$c/2)/2=$d";
+    echo "<hr>";
+
+
+?>
